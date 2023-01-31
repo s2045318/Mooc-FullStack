1793642cb48137384209725eb5bfec013320f41f
s2045318 <s2045318@ed.ac.uk>
2023-01-31 13:36:05 +0100

    1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,12 +5,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  console.log("Application started")
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
+
+      <h1>statistics</h1>
+      <p>
+        good {good} <br/>
+        neutral {neutral} <br/>
+        bad {bad}
+      </p>
     </div>
   )
 }
+
 
 export default App
