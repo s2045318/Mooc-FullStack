9ed58b20919c28de4640b11c6bb0b031bab6ec44
s2045318 <s2045318@ed.ac.uk>
2023-02-08 19:30:58 +0200

    1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,57 +1,68 @@
 import { useState } from 'react'
 
+// a proper place to define a component
+const Statistics = (props) => {
+
+  const good = props.good
+  const bad = props.bad
+  const neutral = props.neutral
+  const total = good + bad + neutral
+
+  const findAverage = (good - bad) / (total)
+  const findPositive = 100 * good / total
+  if (total == 0) { 
+    console.log('page started')
+    return (
+      <div>
+      <h2>No feedback given</h2>
+      </div>
+   )
+  } else {
+    console.log('page updated')
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>good {good} <br/>
+        bad {bad}<br/>
+        neutral {neutral} <br/>
+        all {total} <br/>
+        average {findAverage} <br/>
+        positive {findPositive} %
+        </p>
+      </div>
+    ) 
+  }
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
+  
+  const increaseGood = () => setGood(good+1)
+  const increaseBad = () => setBad(bad+1)
+  const increaseNeutral = () => setNeutral(neutral+1)
+  
+  // do not define a component within another component
 
 
-
-  const increaseGood = () => {
-    setGood(good + 1)
-    setTotal(total + 1)
-    console.log('good increased')
-  }
-  const increaseNeutral = () => {
-    setNeutral(neutral + 1)
-    setTotal(total + 1)
-    console.log('good increased')
-  }
-  const increaseBad = () => {
-    setBad(bad + 1)
-    setTotal(total + 1)
-    console.log('good increased')
-  }
-  console.log('application (re)started')
-//  ,  setAvg(findavg(good,bad,total))
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={increaseGood}>
-        good
-      </button>
-      <button onClick={increaseNeutral}>
-        neutral
-      </button>
-      <button onClick={increaseBad}>
-        bad
-      </button>
-
-      <h1>statistics</h1>
-      <p>
-        good {good} <br/>
-        neutral {neutral} <br/>
-        bad {bad} <br/>
-        all {total} <br/>
-        average {findAverage(good,bad,total)} <br/>
-        positive {findPositive(good, total)} %
-      </p>
-    </div>
+        <button onClick={increaseGood}>
+          good
+        </button>
+        <button onClick={increaseNeutral}>
+          neutral
+        </button>
+        <button onClick={increaseBad}>
+          bad
+        </button>
+        <h1>statistics</h1>
+        <Statistics good={good} bad={bad} neutral={neutral}/>
+      </div>
   )
 }
-const findAverage = (good,bad,total) => (good - bad) / (total)
-const findPositive = (good,total) => 100 * good / total
+
 
 export default App
